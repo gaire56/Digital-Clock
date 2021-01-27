8303869eb562401087ab8414f9643c13748006d7
Anant Gaire <56.anant@gmail.com>
2021-01-27 14:38:42 +0545

    get local machine time

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const time = new Date().toLocaleTimeString();
+
+  const [currentTime, setCurrentTime] = useState(time);
   return (
     <div className="app">
-      <h1>Hi from App</h1>
+      <h1>{currentTime}</h1>
     </div>
   );
-}
+};
 
 export default App;
