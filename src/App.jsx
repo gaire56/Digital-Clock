e85d220109764791d3908c1c21cf4151145520dc
Anant Gaire <56.anant@gmail.com>
2021-01-27 14:57:13 +0545

    Comments addaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  let time = new Date().toLocaleTimeString();
+  //creat aero function
+  let time = new Date().toLocaleTimeString(); // get local machine time
 
-  const [currentTime, setCurrentTime] = useState(time);
+  const [currentTime, setCurrentTime] = useState(time); // hooks using useState
 
   const UpdateTime = () => {
-    time = new Date().toLocaleTimeString();
-    setCurrentTime(time);
+    // create function called UpdateTiem to update on screen
+    time = new Date().toLocaleTimeString(); //call local machine time
+    setCurrentTime(time); // change current time into time
   };
 
-  setInterval(UpdateTime, 1000);
+  setInterval(UpdateTime, 1000); // using setInterval for fetch time second by second
   return (
     <div className="app">
-      <h1>{currentTime}</h1>
+      {' '}
+      {/*div created */}
+      <h1>{currentTime}</h1> {/*using js expression for display time */}
     </div>
   );
 };
 
-export default App;
+export default App; // Export app
